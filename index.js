88ba9771b333dfe7022c0cc22524178dc8cbcbf7
TH-Raju <rajukhan894200@gmail.com>
2022-11-24 16:46:29 +0600

    mongodb connected and data loaded

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -9,6 +10,36 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ddpko0x.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categoriyCollection = client.db('resaleHanding').collection('categoriy')
+
+        app.get('/categoriy', async (req, res) => {
+            const query = {};
+            const options = await categoriyCollection.find(query).toArray();
+            res.send(options);
+        })
+
+        app.get('/categoriy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await categoriyCollection.findOne(query);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
+
 
 app.get('/', async (req, res) => {
     res.send('Server is working...');
